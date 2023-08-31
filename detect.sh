c0d7d183cfb2a3885d6dbbb7dd99bd6408d58fbc
diditeko <diditekosetyo@gmail.com>
2023-08-31 11:34:28 +0700

    v1

---
+++ detect.sh
@@ -0,0 +1,1 @@
+/c/Users/didit/anaconda3/envs/captcha/python detect.py --model_def config/yolov3-captcha.cfg --weights_path checkpoints/yolov3_ckpt_1.pth --image_folder data/captcha/test --class_path data/captcha/classes.names
---
+++ train.sh
@@ -0,0 +1,1 @@
+/c/Users/didit/anaconda3/envs/captcha/python train.py --model_def config/yolov3-captcha.cfg --data_config config/captcha.data --pretrained_weights weights/darknet53.conv.74
